3f3079192176b77241c941ce8554aab84a4deab7
RIfkiND <122434422+RIfkiND@users.noreply.github.com>
2023-01-17 22:16:33 +0700

    Add files via upload

---
+++ Atas.h
@@ -0,0 +1,14 @@
+header(char judd[50]="")
+{
+if(strcmp(judd,"HEADER")==0)
+{
+cout<<"===================Biodata====================="<<endl;
+cout<<"Nama  : RIFKI N D"<<endl;
+cout<<"Kelas : X-PPLG"<<endl;
+cout<<"User  : Rifki1"<<endl;
+cout<<"Pass  : kaka1234"<<endl;
+cout<<"=============Masukan User dan Pass=============="<<endl;
+getch();
+}
+}
+
